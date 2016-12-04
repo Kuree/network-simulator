1e4b0f0c734534ebc00e26d212df621cabcc87ac
Keyi Zhang <kz005@bucknell.edu>
2016-12-04 16:12:55 -0500

    moved to Python 3.5

--- tests/bootstrap.py
+++ tests/bootstrap.py
@@ -1,4 +1,3 @@
-if __package__ is None:
-    from os import sys, path
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
